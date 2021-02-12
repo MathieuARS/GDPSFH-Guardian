02004539644d93f7e17989198f75c376a210aab4
MathieuARS <mathieu.maik.15@gmail.com>
2021-02-12 09:54:01 +0100

    Add files via upload

--- Guardian.py
+++ Guardian.py
@@ -13,7 +13,8 @@ async def on_message(message):
     noemoji = "<:no:809571073531183144>"
     maybeemoji = "<:maybe:809572355533701131>"
     wrongchannelerror = "You are asking for help in the wrong channel. Go to <#743037416947974165>"
-    helpchannel = 743037416947974165
+    help_channel = 743037416947974165
+    owner_id = 195598321501470720
 
     pingmsglist = ["https://tenor.com/view/pinged-sargent-who-pinged-me-gif-15913755",
                   "https://tenor.com/view/ping-slap-dog-doggo-punch-gif-17672413",
@@ -41,17 +42,17 @@ async def on_message(message):
     elif "765571424903233577" in message.content:
         await message.channel.send("".join([pingmsglist[random.randrange(0, len(pingmsglist))] for i in range(1)]))
     # Ping mathieu message
-    elif "195598321501470720" in message.content:
+    elif str(owner_id) in message.content:
         await message.channel.send("I hope you had a good reason to ping the owner..")
     # !c in non verified chat message
     elif "!c" in message.content:
-        if message.channel.id == 803605671580270623: # non-verified-chat
+        if message.channel.id == 803605671580270623: # non-verified-chat id
             await message.channel.send("You need to be verified first before doing that command.")
     # Get mod message
     elif ("get mod" in message.content or
          "give mod" in message.content or
          "myself mod" in message.content ):
-        if message.channel.id != helpchannel:
+        if message.channel.id != help_channel:
             await message.channel.send(wrongchannelerror)
         else:
             await message.channel.send("You can look in <#743030590005051434> if you want to put yourself as owner or look in <#746464567734829168> to give someone a role.")
@@ -63,7 +64,7 @@ async def on_message(message):
          "chest reward" in message.content or
          "rewards of chest" in message.content or
          "rewards of the chest" in message.content):
-        if message.channel.id != helpchannel:
+        if message.channel.id != help_channel:
             await message.channel.send(wrongchannelerror)
         else:
             await message.channel.send("To change your chests rewards go on your gdps tools (http://gdps.mathieuar.fr/<yourgdps>/tools/changeChestRewards.php) and just fill up the informations.")
@@ -72,7 +73,7 @@ async def on_message(message):
          "create a gauntlet" in message.content or
          "make gauntlet" in message.content or
          "make a gauntlet" in message.content):
-        if message.channel.id != helpchannel:
+        if message.channel.id != help_channel:
             await message.channel.send(wrongchannelerror)
         else:
             await message.channel.send("You can look in <#743027678046846999>.")
@@ -81,7 +82,7 @@ async def on_message(message):
          "create a map pack" in message.content or
          "make map pack" in message.content or
          "make a map pack" in message.content):
-        if message.channel.id != helpchannel:
+        if message.channel.id != help_channel:
             await message.channel.send(wrongchannelerror)
         else:
             await message.channel.send("You can look in <#743027631968223284>.")
@@ -90,20 +91,20 @@ async def on_message(message):
          "create a gdps" in message.content or
          "make gdps" in message.content or
          "make a gdps" in message.content):
-        if message.channel.id != helpchannel:
+        if message.channel.id != help_channel:
             await message.channel.send(wrongchannelerror)
         else:
             await message.channel.send("To create a gdps you need to be level 1+ and type !c in <#748238238736711761>")
     # Delete gdps message
     elif ("delete gdps" in message.content or
          "delete my gdps" in message.content):
-        if message.channel.id != helpchannel:
+        if message.channel.id != help_channel:
             await message.channel.send(wrongchannelerror)
         else:
             await message.channel.send("To delete your gdps you can look at the pinned messages in <#743037447218397215>.")
     # Account disabled message
     elif ("account disabled" in message.content):
-        if message.channel.id != helpchannel:
+        if message.channel.id != help_channel:
             await message.channel.send(wrongchannelerror)
         else:
             await message.channel.send("That means that you got **auto banned**, to unban yourself __delete everything__ in the \"Actions\" Table > click your database > click on the actions table > operations button on top > at the bottom click __<Clear table (TRUNCATE)>__.")
@@ -112,7 +113,7 @@ async def on_message(message):
          "lost my account password" in message.content or
          "forgot my password" in message.content or
          "forgot my account password" in message.content):
-        if message.channel.id == helpchannel:
+        if message.channel.id == help_channel:
             await message.channel.send("If you lost your password login in your database then go in the accounts table, look for your account and replace whats in password by **$2y$10$NcGFx6QGceNBdWWigg9A2.z9NfY5czyhEb0y0GijRurPI7rv71Liu**\n\nThis will change your password to 123456 but be sure to change it again in your gdps tools.")
 
 @client.event
