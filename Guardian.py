eb6d16db20ce3b889739cf77969746a39ae43c9a
MathieuARS <mathieu.maik.15@gmail.com>
2021-06-22 10:24:17 +0200

    Update Guardian.py
    Delete a message if it contains stearncomminuty (A scam)

--- Guardian.py
+++ Guardian.py
@@ -1,14 +1,19 @@
 import random
 import discord
+import re
+import os
 from discord.ext import commands
-from discord.utils import get
+from dotenv import load_dotenv, find_dotenv
 
+load_dotenv(find_dotenv())
+bot_token = os.environ.get("bot_token")
 prefix = "this_is_useless_dont_touch_that"
 client = commands.Bot(command_prefix = prefix, help_command=None)
-txt = open("bot_token.txt", "r")
-bot_token = txt.readline()
 
 owner_id = 195598321501470720
+owners_id = [195598321501470720, #Moi
+             180790976128745472] #Rya
+
 suggestions_channels = [745331693396820090, # suggestions
                         797968735632752660, # stats-bot-suggestion
                         793552899983540254, # manager-suggestion
@@ -17,6 +22,10 @@ suggestions_channels = [745331693396820090, # suggestions
 
 @client.event
 async def on_message(message):
+    x = re.search(r"(s.{0,4}t.{0,4}e.{0,4}a.{0,4}r.{0,4}n.{0,4}c.{0,4}o.{0,4}m.{0,4}m.{0,4}i.{0,4}n.{0,4}u.{0,4}t.{0,4}y).*", message.content)
+    if x is not None:
+        await message.delete()
+
     ping_owner_message = False
     yesemoji = "<:yes:809571059941769247>"
     noemoji = "<:no:809571073531183144>"
@@ -37,6 +46,14 @@ async def on_message(message):
                   "For real can you please stop pinging me?",
                   "What do you want???",
                   "<:ping:746467200453574799>"]
+    blacklisted_channels = [743030923678711808, #announcements
+                            802655265366212698, #mod-announcements
+                            765369648971382784, #down-time
+                            809420779225350144] #presentations
+    creategdps_wordlist = ["create gdps",
+                           "create a gdps",
+                           "make gdps",
+                           "make a gdps"]
 
     # Suggestions channels reactions
     if message.channel.id in suggestions_channels:
@@ -46,18 +63,17 @@ async def on_message(message):
             await message.add_reaction(yesemoji)
             await message.add_reaction(noemoji)
             await message.add_reaction(maybeemoji)
+    if message.channel.id in blacklisted_channels:
+        return
     # Ping messages
     elif "765571424903233577" in message.content:
         await message.channel.send("".join([pingmsglist[random.randrange(0, len(pingmsglist))] for i in range(1)]))
+    # Ping owner message
     elif str(owner_id) in message.content:
         if ping_owner_message is True:
             await message.channel.send("I hope you had a good reason to ping the owner..")
         else:
             pass
-    # !c in non verified chat message
-    elif "!c" in message.content:
-        if message.channel.id == 803605671580270623: # non-verified-chat id
-            await message.channel.send("You need to be verified first before doing that command.")
     # Get mod message
     elif ("get mod" in message.content or
          "give mod" in message.content or
@@ -97,10 +113,7 @@ async def on_message(message):
         else:
             await message.channel.send("You can look in <#743027631968223284>.")
     # Create gdps message
-    elif ("create gdps" in message.content or
-         "create a gdps" in message.content or
-         "make gdps" in message.content or
-         "make a gdps" in message.content):
+    elif message.content in creategdps_wordlist:
         if message.channel.id != help_channel:
             await message.channel.send(wrongchannelerror)
         else:
@@ -112,12 +125,6 @@ async def on_message(message):
             await message.channel.send(wrongchannelerror)
         else:
             await message.channel.send("To delete your gdps you can look at the pinned messages in <#743037447218397215>.")
-    # Account disabled message
-    elif ("account disabled" in message.content):
-        if message.channel.id != help_channel:
-            await message.channel.send(wrongchannelerror)
-        else:
-            await message.channel.send("That means that you got **auto banned**, to unban yourself __delete everything__ in the \"Actions\" Table > click your database > click on the actions table > operations button on top > at the bottom click __<Clear table (TRUNCATE)>__.")
     # Lost account password message
     elif ("lost my password" in message.content or
          "lost my account password" in message.content or
@@ -125,6 +132,12 @@ async def on_message(message):
          "forgot my account password" in message.content):
         if message.channel.id == help_channel:
             await message.channel.send("If you lost your password login in your database then go in the accounts table, look for your account and replace whats in password by **$2y$10$NcGFx6QGceNBdWWigg9A2.z9NfY5czyhEb0y0GijRurPI7rv71Liu**\n\nThis will change your password to 123456 but be sure to change it again in your gdps tools.")
+
+@client.event
+async def on_message_edit(before, after):
+    x = re.search(r"(s.{0,4}t.{0,4}e.{0,4}a.{0,4}r.{0,4}n.{0,4}c.{0,4}o.{0,4}m.{0,4}m.{0,4}i.{0,4}n.{0,4}u.{0,4}t.{0,4}y).*", after.content)
+    if x is not None:
+        await after.delete()
 
 @client.event
 async def on_raw_reaction_add(payload):
@@ -141,19 +154,22 @@ async def on_raw_reaction_add(payload):
     maybe = discord.Embed(title="Suggestion may be accepted.", description = "<@" + authorid + ">", color = discord.Colour.from_rgb(255, 128, 0))
 
     if payload.channel_id in suggestions_channels:
-        if payload.user_id == owner_id:
+        if payload.user_id in owners_id:
             if payload.emoji == yesemoji:
                 accepted.add_field(name = "Suggestion:", value = suggestion_content, inline = True)
                 await channel.send(embed=accepted)
+                await msg.delete()
             elif payload.emoji == noemoji:
                 denied.add_field(name = "Suggestion:", value = suggestion_content, inline = True)
                 await channel.send(embed=denied)
+                await msg.delete()
             elif payload.emoji == maybeemoji:
                 maybe.add_field(name = "Suggestion:", value = suggestion_content, inline = True)
                 await channel.send(embed=maybe)
+                await msg.delete()
 
 @client.event
 async def on_ready():
-    print("Bot pret")
+    print("Bot ready")
 
 client.run(bot_token)
